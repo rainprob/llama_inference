a32ddc20eeebe524220cb2203c3b26a23e4c75cc
haoxuan <haoxuanx@hotmail.com>
2024-06-28 20:48:21 +0800

    added calPaddingoffset kernel

---
+++ build/src/kernels/CMakeFiles/calPaddingoffsetFunctor.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../../lib/libcalPaddingoffsetFunctor.a"
+  "../../lib/libcalPaddingoffsetFunctor.pdb"
+  "CMakeFiles/calPaddingoffsetFunctor.dir/cal_paddingoffset.cu.o"
+  "CMakeFiles/calPaddingoffsetFunctor.dir/cal_paddingoffset.cu.o.d"
+  "CMakeFiles/calPaddingoffsetFunctor.dir/cmake_device_link.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/calPaddingoffsetFunctor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/tests/unittests/CMakeFiles/test_cal_paddingoffset.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../bin/test_cal_paddingoffset"
+  "../../bin/test_cal_paddingoffset.pdb"
+  "CMakeFiles/test_cal_paddingoffset.dir/test_cal_paddingoffset.cu.o"
+  "CMakeFiles/test_cal_paddingoffset.dir/test_cal_paddingoffset.cu.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/test_cal_paddingoffset.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
