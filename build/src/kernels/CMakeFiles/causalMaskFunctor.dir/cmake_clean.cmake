71cb5d307f5ae15a37fca454ed2728668d72f217
haoxuan <haoxuan.xu@hotmail.com>
2024-06-29 18:40:05 +0800

    added mask kernel

---
+++ build/src/kernels/CMakeFiles/causalMaskFunctor.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../../lib/libcausalMaskFunctor.a"
+  "../../lib/libcausalMaskFunctor.pdb"
+  "CMakeFiles/causalMaskFunctor.dir/causal_mask.cu.o"
+  "CMakeFiles/causalMaskFunctor.dir/causal_mask.cu.o.d"
+  "CMakeFiles/causalMaskFunctor.dir/cmake_device_link.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/causalMaskFunctor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/tests/unittests/CMakeFiles/causalmask.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../bin/causalmask"
+  "../../bin/causalmask.pdb"
+  "CMakeFiles/causalmask.dir/test_casual_mask.cu.o"
+  "CMakeFiles/causalmask.dir/test_casual_mask.cu.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/causalmask.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
